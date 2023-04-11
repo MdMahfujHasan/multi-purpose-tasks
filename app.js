0c2c10cbda689316433299ab47c11ab68ef5356d
Mahfuj Munna <mahfujmunna17@gmail.com>
2023-04-11 22:48:17 +0600

    footer added, finished

--- app.js
+++ app.js
@@ -32,3 +32,30 @@ hideOnPointerLeave('img-7', 'img-7-p');
 
 displayOnPointerEnter('img-8', 'img-8-p');
 hideOnPointerLeave('img-8', 'img-8-p');
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
